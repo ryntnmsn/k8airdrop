3a905d0e9ac5215da468c11a305a3d1ed87d6d13
ryntnmsn <ryn.tnmsn@gmail.com>
2023-12-21 11:20:39 +0800

    commit

--- resources/views/livewire/platforms.blade.php
+++ resources/views/livewire/platforms.blade.php
@@ -1,10 +1,30 @@
 <div class="mt-10">
-    @if($platforms->count() != null)
-
-        <div>
-            <x-input-text wire:model.live='search' placeholder="Seach here..."></x-input-text>
+    <div class="flex space-x-5">
+        <div class="flex-1 relative mb-5">
+            <x-input-text wire:model.live='search' placeholder="Seach here..." class="pl-8 mb-0"></x-input-text>
+            <div class="absolute left-0 top-0 flex items-center h-full pl-2">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" data-slot="icon" class="w-5 h-5 text-slate-400">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+                </svg>
+            </div>
         </div>
+        <div class="flex-1">
+            <div class="flex items-center float-right space-x-3">
+                <span>
+                    Show records:
+                </span>
+                <span>
+                    <select wire:model.live='pagination' name="" id="" class="border-slate-200 rounded-lg">
+                        <option value="5">5</option>
+                        <option value="10">10</option>
+                        <option value="20">20</option>
+                    </select>
+                </span>
+            </div>
+        </div>
+    </div>
 
+    @if($platforms->count() != null)
         <div class="relative overflow-x-auto sm:rounded-lg">
             <table class="w-full text-left rtl:text-right rounded-2xl text-slate-700 ">
                 <thead class="text-xs uppercase bg-slate-100">
