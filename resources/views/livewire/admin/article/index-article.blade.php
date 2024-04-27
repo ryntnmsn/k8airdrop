969f04c3c6b7af0d32cb80eb048b4eebd2b1749f
ryntnmsn <ryn.tnmsn@gmail.com>
2024-04-27 10:07:45 +0800

    Commit

--- resources/views/livewire/admin/article/index-article.blade.php
+++ resources/views/livewire/admin/article/index-article.blade.php
@@ -5,7 +5,7 @@
             <x-text-helper>Manage news by using operations create, update and delete.</x-text-helper>
         </div>
         <div class="flex-1">
-            <x-href wire:navigate href="{{ route('articles.create') }}" >
+            <x-href href="{{ route('articles.create') }}" >
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" data-slot="icon" class="w-6 h-6">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
                 </svg>
