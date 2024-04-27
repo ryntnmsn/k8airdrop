ff05329ae93ca866d3d878bc7041850b07d075a7
ryntnmsn <ryn.tnmsn@gmail.com>
2024-04-27 16:13:23 +0800

    Commit

--- resources/views/livewire/admin/article/edit-article.blade.php
+++ resources/views/livewire/admin/article/edit-article.blade.php
@@ -64,7 +64,7 @@
                     @enderror
                 </div>
 
-                <div wire:ignore.self>
+                <div wire:ignore>
                     <x-label>Description</x-label>
                     <x-textarea wire:model="description" class="w-full !h-40" id="description"></x-textarea>
                     @error('description')
