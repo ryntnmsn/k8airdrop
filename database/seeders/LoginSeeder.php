b02b85c2a6ffce5d5fb63a58bce8ce49a7039fa2
ryntnmsn <ryn.tnmsn@gmail.com>
2024-04-27 09:37:24 +0800

    commit

---
+++ database/seeders/LoginSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class LoginSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('users')->insert([
+            [
+                'name' => 'K8',
+                'email' => 'k8casinomkt@gmail.com',
+                'k8_username' => 'k8admin',
+                'password' => Hash::make('k8admin123'),
+            ],
+            [
+                'ronin' => 'Ronin',
+                'email' => 'ronin@mail-k8.io',
+                'k8_username' => 'ronink8',
+                'password' => Hash::make('k8admin123'),
+            ]
+        ]);
+    }
+}
