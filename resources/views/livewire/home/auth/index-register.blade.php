47db85b643af28b5fd5150146751c3d0d160d145
ryntnmsn <ryn.tnmsn@gmail.com>
2024-05-03 16:24:18 +0800

    Commit

--- resources/views/livewire/home/auth/index-register.blade.php
+++ resources/views/livewire/home/auth/index-register.blade.php
@@ -13,7 +13,7 @@
                                     <path fill-rule="evenodd" d="M18.685 19.097A9.723 9.723 0 0 0 21.75 12c0-5.385-4.365-9.75-9.75-9.75S2.25 6.615 2.25 12a9.723 9.723 0 0 0 3.065 7.097A9.716 9.716 0 0 0 12 21.75a9.716 9.716 0 0 0 6.685-2.653Zm-12.54-1.285A7.486 7.486 0 0 1 12 15a7.486 7.486 0 0 1 5.855 2.812A8.224 8.224 0 0 1 12 20.25a8.224 8.224 0 0 1-5.855-2.438ZM15.75 9a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z" clip-rule="evenodd" />
                                   </svg>
                                 </div>
-                            <x-input-text wire:model.live="name" class="!bg-slate-800 border-none !text-slate-200 !ps-12 !p-4 font-bold focus:ring-indigo-600 focus:ring-2 placeholder-slate-600" placeholder="{{ __('Enter nick name') }}"></x-input-text>
+                            <x-input-text wire:model.live="name" class="!bg-slate-800 border-none !text-slate-200 !ps-12 !p-4 font-bold focus:ring-indigo-600 focus:ring-2 placeholder-slate-600" placeholder="{{ __('Enter nickname') }}"></x-input-text>
                         </div>
                         @error('name')
                             <span class="text-sm text-rose-500 pt-2 block">{{ __($message) }}</span>
