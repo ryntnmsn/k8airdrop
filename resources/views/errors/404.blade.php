866f6f08dab4ec4ac085b8da4916f3f26f53999f
ryntnmsn <ryn.tnmsn@gmail.com>
2025-08-01 12:27:13 +0800

    commit

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -9,10 +9,10 @@
         <div><h1 class="text-9xl fw-bold text-blue-100">404</h1></div>
         <div><p class="fw-bold text-blue-100 text-center">{{ __('Page not found') }}</p></div>
         <div class="mt-10">
-            <a class="block bg-indigo-600 fw-bold text-slate-100 py-2 rounded-md text-center hover:-translate-y-1 duration-500 hover:shadow-xl" href="/">{{ __('Go back to promos') }}</a>
+            <a class="block bg-[#a38e5c] fw-bold text-slate-100 py-2 rounded-full text-center hover:-translate-y-1 duration-500 hover:shadow-xl" href="/">{{ __('Go back to promos') }}</a>
         </div>
     </div>
-    
+
 </div>
-    
+
 @stop
